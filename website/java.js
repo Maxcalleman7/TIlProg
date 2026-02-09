f3e46914dbbcf09d3b3eaba48ff2a27cd55fc5a7
Cale_exe <82705349+Maxcalleman7@users.noreply.github.com>
2026-02-09 14:39:00 +0100

    tillåtna tecken fixat

--- website/java.js
+++ website/java.js
@@ -1,20 +1,65 @@
 const namn = "java"; 
 console.log("Welcome to " + namn + " programming!");
 
+let message = document.getElementById("message");
+let username = document.getElementById("username");
+
+
+
+username = username.value.toUpperCase();
+message = message.value;
+
+const charsChecker = (text) => {
+const okChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz,?. ";
+let text = username + ": " + message;
+let valid = true;
+
+for (let i = 0; i < text.length; i++) {
+  if (!okChars.includes(text[i])) {
+    valid = false;
+    break;
+  }
+}
+
+if (!valid) {
+  alert("Meddelandet innehåller ogiltiga tecken.");
+}
+
+for (let i = 0; i < text.length; i++) {
+  if (text[i] === "username:" || text[i] === "message:") {
+    text[i] = ""; 
+  }
+}
+}
+
+
+
+
+
+
 let chatInput = document.getElementById("chatInput");
-let usermsg = document.getElementById("usermsg");
+
 
 chatInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        usermsg.textContent = ">> " + chatInput.value + ": ";
+        username.textContent = ">> " + chatInput.value + ": ";
         chatInput.value = "";
     }
 
-    if (usermsg.textContent.length > 20) {
-    alert("Meddelandet får inte vara längre än 20 tecken.");
-    usermsg.textContent = ">> : ";
+    if (username.textContent.length > 20) {
+    alert("Name får inte vara längre än 20 tecken.");
+    username.textContent = ">> : ";
 }
 });
+
+
+
+
+
+
+
+
+
 
 
 
