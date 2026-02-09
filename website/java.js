627490c3885f3e99228c24c0de12c61f92ef3267
Cale_exe <82705349+Maxcalleman7@users.noreply.github.com>
2026-02-09 13:32:06 +0100

    Add chat functionality and style adjustments
    - Implemented user message display in the chat section.
    - Added event listener for chat input to handle message submission.
    - Updated CSS to improve layout and prevent overflow.

--- website/java.js
+++ website/java.js
@@ -1,2 +1,27 @@
 const namn = "java"; 
 console.log("Welcome to " + namn + " programming!");
+
+let chatInput = document.getElementById("chatInput");
+let usermsg = document.getElementById("usermsg");
+
+chatInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        usermsg.textContent = ">> " + chatInput.value + ": ";
+        chatInput.value = "";
+    }
+
+    if (usermsg.textContent.length > 20) {
+    alert("Meddelandet får inte vara längre än 20 tecken.");
+    usermsg.textContent = ">> : ";
+}
+});
+
+
+
+
+
+
+
+
+
+
