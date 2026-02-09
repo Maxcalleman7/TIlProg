465c7e418959d89d82010804ee8976e3aa3813b8
cj <cjj@bahnhof.se>
2026-02-09 14:10:17 +0100

    lagt till colon i meddelandeformattering

--- Skrivapp/Skrivapp/Program.cs
+++ Skrivapp/Skrivapp/Program.cs
@@ -16,7 +16,7 @@
         message = Console.ReadLine().ToUpper();
 
 
-        morseMessage = "Filetype: Flipper Music Format\r\nVersion: 0 \r\nBPM: 120 \r\nDuration: 8\r\nOctave: 4\r\nNotes:" + Translate($"username {userName}") + " " + Translate($"message {morseMessage}");
+        morseMessage = "Filetype: Flipper Music Format\r\nVersion: 0 \r\nBPM: 120 \r\nDuration: 8\r\nOctave: 4\r\nNotes:" + Translate($"username:{userName}") + " " + Translate($"message:{morseMessage}");
 
         File.WriteAllText(filepath, morseMessage);
     }
