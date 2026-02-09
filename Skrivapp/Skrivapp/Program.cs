27943291e0aa01cf7869709f3d6859f31ae98946
cj <cjj@bahnhof.se>
2026-02-09 13:40:29 +0100

    lagt till indikatorsom skiljer meddelandet från användarnamnet

--- Skrivapp/Skrivapp/Program.cs
+++ Skrivapp/Skrivapp/Program.cs
@@ -13,10 +13,10 @@
         userName = Console.ReadLine();
 
         Console.WriteLine("enter message");
-         message = Console.ReadLine().ToUpper();
+        message = Console.ReadLine().ToUpper();
 
         
-        morseMessage = "Filetype: Flipper Music Format\r\nVersion: 0 \r\nBPM: 120 \r\nDuration: 8\r\nOctave: 4\r\nNotes:"+ Translate($"username {userName}") + Translate(morseMessage);
+        morseMessage = "Filetype: Flipper Music Format\r\nVersion: 0 \r\nBPM: 120 \r\nDuration: 8\r\nOctave: 4\r\nNotes:"+ Translate($"username {userName}") + Translate($"message {morseMessage}");
 
         File.WriteAllText(filepath, morseMessage);
     }
