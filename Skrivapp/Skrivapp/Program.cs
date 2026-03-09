78b535ccd21901476d453ccc56dfb13a0e6693d5
cj <cjj@bahnhof.se>
2026-03-09 13:10:02 +0100

    fixat nummer i charlistan

--- Skrivapp/Skrivapp/Program.cs
+++ Skrivapp/Skrivapp/Program.cs
@@ -115,7 +115,19 @@
             ['Y'] = Y,
             ['Z'] = Z,
 
-            
+            ['1'] = one,
+            ['2'] = two,
+            ['3'] = three,
+            ['4'] = four,
+            ['5'] = five,
+            ['6'] = six,
+            ['7'] = seven,
+            ['8'] = eight,
+            ['9'] = nine,
+            ['0'] = zero,
+
+
+
 
             [' '] = wordspace,
 
