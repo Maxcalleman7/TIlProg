77bb03a563e3d7bb06bea0a0284bc61bfccf4f6d
cj <cjj@bahnhof.se>
2026-04-27 14:44:32 +0200

    lagt till filerna

--- Server3/private/java.js
+++ Server3/private/java.js
@@ -1,12 +1,18 @@
 const namn = "java"; 
 console.log("Welcome to " + namn + " programming!");
+const fs= require ('fs');
+const filepath='Msgs.txt';
+const content = fs.readFileSync(filepath, 'utf-8');
 
 let message = document.getElementById("message");
 let chat = document.getElementById("chat"); 
 let usernameIL = document.getElementById("username-Inlg"); 
 let userDisplay = document.getElementById("userDisplay"); 
+let msgTest=document.getElementById("msgTest");
 
 let chatInput = document.getElementById("chatInput"); //Vi ska senare använda texten från chatloggen senare. 
+
+msgTest.textContent=content;
 
 // Hämta användarnamn när sidan laddas
 fetch('/api/user')
@@ -24,6 +30,7 @@ chatInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         chat.textContent +=   ">>: " + chatInput.value; 
         chatInput.value = "";
+        
     } 
 
 });
