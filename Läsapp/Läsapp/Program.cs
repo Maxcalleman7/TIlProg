96a64f2bae7544ac133d486904ee84c21a93dc9d
cj <cjj@bahnhof.se>
2026-05-18 14:39:28 +0200

    Readapp klar, delen som hämtar från fldigi ostabil

--- Läsapp/Läsapp/Program.cs
+++ Läsapp/Läsapp/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.IO;
+﻿using System.Data.SQLite;
+using System.IO;
 using System.Linq.Expressions;
-using System.Data.SQLite;
+using System.Xml.Linq;
 
 static class Program
 {
@@ -38,12 +39,33 @@ static class Program
         public void AddMessage(string user, string msg)
         {
             Open();
-            string sqlcommand = $"INSERT INTO msgs()";
+            string sqlcommand = $"INSERT INTO msgLog(user, message) VALUES (@user, @msg);";
+            _command.CommandText = sqlcommand;
+
+            SQLiteParameter userParam = new SQLiteParameter("@user", System.Data.DbType.String);
+            SQLiteParameter msgParam = new SQLiteParameter("@msg", System.Data.DbType.String);
+            
+
+            userParam.Value = user;
+            msgParam.Value = msg;
+            
+
+            _command.Parameters.Add(userParam);
+            _command.Parameters.Add(msgParam);
+            
+
+            _command.Prepare();
+            _command.ExecuteNonQuery();
+            Close();
         }
 
     }
     public static void Main()
     {
+        bool readingMsg = false;
+
+        string msg = "";
+
         DateTime dateTime = DateTime.Now;
 
         string date = $"{dateTime.Year}";//fldigi separerar loggar med datum, så den behöver dagens datum för att hitta loggen från samma dag
@@ -64,17 +86,16 @@ static class Program
             date += $"{dateTime.Day}";
         }
 
-        bool readingMsg = false;
-
-        string msg = "";
+        
 
         List<string> list = new List<string>();
 
-        //string logFilePath = $"C:\\Users\\cjj\\fldigi.files\\fldigi20260316.log";
+        //string logFilePath = $"C:\\Users\\cjj\\fldigi.files\\fldigi20260518.log";//för test
         string logFilePath = $"../../../fldigi.files/fldigi{date}.log";
 
 
-        //skapar en kopia av fldigi-logen för att kunna läsa texten i den
+        //skapar en kopia av fldigi-logen för att kunna läsa texten i den, ger errors om man inte har fldigi så är utkommenterad här
+        /*
         using (var inputFile = new FileStream(
             logFilePath,
             FileMode.Open,
@@ -92,7 +113,7 @@ static class Program
                 }
             }
         }
-
+        */
 
 
         string writeFilePath = "MessageLog.txt";
@@ -101,7 +122,7 @@ static class Program
 
         using (StreamReader sr = new StreamReader("fldigiLogCopy.txt"))
         {
-            while (sr.Peek() >= 0)
+            while (sr.Peek() >= 0) //kollar så att nästa char inte är null
             {
                 char c = (char)sr.Read();
                 if (c == '>' && readingMsg == false) //när meddelandet i logen börjar, börja spara meddelandet
@@ -131,41 +152,83 @@ static class Program
                 sw.WriteLine(messages[i]); //varje meddelande är 1 Line, sparas som text
             }
         }
+        //exempel på output:
+        
+
+        
 
 
 
+        
 
         List<string> msgTexts = new List<string>();
         List<string> msgUsers = new List<string>();
 
         
+
         string user = "";
         string msgText = "";
+
+        bool savedUser = false;
+        bool readingUser = false;
+        readingMsg=false;
 
         using (StreamReader sr = new StreamReader("MessageLog.txt")) //de sparade meddelandena sparas i en sqlite fil med meddelandet och den tänkta mottagaren.
         {
             while (sr.Peek() >= 0)
             {
                 char c = (char)sr.Read();
-                if (c == ':' && readingMsg == false) 
+                if (c == ':' && readingUser == false&&savedUser==false) //när usernamet börjar, läs in usernamnet
                 {
-                    readingMsg = true;
+                    readingUser = true;
 
                 }
-                else if (c == ' ') 
+                else if (c == ' '&&readingUser==true) //när usernamet är slut, spara usern
                 {
-                    readingMsg = false;
-                    user += c;
+                    readingUser = false;
+                    
                     msgUsers.Add(user);
+                    savedUser = true;
                     user = "";
                 }
-
-                if (readingMsg == true) 
+                else if (readingUser == true) 
                 {
                     user += c;
+                }
+
+
+                if (c == ':' &&savedUser==true && readingMsg==false)
+                {
+                    readingMsg = true;
+                }
+                else if (c == '<'&&readingMsg==true) //när meddelandet är slut, spara meddelandet
+                {
+                    readingMsg = false;
+
+                    msgTexts.Add(msgText);
+                    
+                    msgText = "";
+                    savedUser = false;
+                }
+                else if (readingMsg == true)
+                {
+                    msgText += c;
                 }
             }
                
+        }
+
+        SQLiteHandler db = new SQLiteHandler("msgs.db");
+
+        bool running = true;
+
+        while (running)
+        {
+            for (int i = 0; i < msgUsers.Count; i++)
+            {
+                db.AddMessage(msgUsers[i], msgTexts[i]);
+            }
+            running = false;
         }
 
 
